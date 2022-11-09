572c17cd2122cd7d37cc8548353e9679f00338c9
92castro <celin92castro@gmail.com>
2022-11-08 22:42:18 -0700

    completed javascript and readme

--- Assets/script.js
+++ Assets/script.js
@@ -14,4 +14,38 @@ $(function () {
 
     localStorage.setItem(blockTime, userInput);
   });
+
+  // current hour in a 24 hour format
+  var currentTime = dayjs().format("h:mm");
+  var timeBlock = $(".time-block");
+  timeBlock.each(function () {
+    var rowTime = $(this).attr("id");
+    //color change according to "past", "present", "future"
+    if (currentTime < rowTime) {
+      // class change to past time
+      $(this).removeClass("present");
+      $(this).removeClass("future");
+      $(this).addClass("past");
+    } else if (currentTime == rowTime) {
+      // class change to present time
+      $(this).removeClass("past");
+      $(this).removeClass("future");
+      $(this).addClass("present");
+    } else {
+      // class change to future time
+      $(this).removeClass("past");
+      $(this).removeClass("present");
+      $(this).addClass("future");
+    }
+  });
+  //saved input according to time slot
+  $("#9 .description").val(localStorage.getItem("9"));
+  $("#10 .description").val(localStorage.getItem("10"));
+  $("#11 .description").val(localStorage.getItem("11"));
+  $("#12 .description").val(localStorage.getItem("12"));
+  $("#13 .description").val(localStorage.getItem("13"));
+  $("#14 .description").val(localStorage.getItem("14"));
+  $("#15 .description").val(localStorage.getItem("15"));
+  $("#16 .description").val(localStorage.getItem("16"));
+  $("#17 .description").val(localStorage.getItem("17"));
 });
--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# work-day-scheduler-
-planner
+# Work-Day-Scheduler
+
+## Task
+
+My task was to built a work day planner where the user can manage their daily tasks. The user is presented with the day, month, and year of when the user access the planner and a 9 to 5 hourly planner. The user can create and save their tasks, as well as access the planner through out the day to remind themselves what needs to get done.
+
+## User Story
+
+AS AN employee with a busy schedule
+I WANT to add important events to a daily planner
+SO THAT I can manage my time effectively
+
+## Acceptance Criteria
+
+GIVEN I am using a daily planner to create a schedule
+WHEN I open the planner
+THEN the current day is displayed at the top of the calendar
+WHEN I scroll down
+THEN I am presented with time blocks for standard business hours
+WHEN I view the time blocks for that day
+THEN each time block is color-coded to indicate whether it is in the past, present, or future
+WHEN I click into a time block
+THEN I can enter an event
+WHEN I click the save button for that time block
+THEN the text for that event is saved in local storage
+WHEN I refresh the page
+THEN the saved events persist
+
+## Review
+
+My github repository page:
+
+- https://github.com/92castro/work-day-scheduler-
+
+My page developed:
+
+- https://92castro.github.io/work-day-scheduler-/
+
+![screenshot of my project](assets/05-third-party-apis-homework-demo.gif)
